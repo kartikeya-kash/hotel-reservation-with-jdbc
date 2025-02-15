8c8bb0de1349c9899f3d4b6a81549ffe715a9f9c
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-15 16:20:13 +0530

    created table

--- hotel_db.sql
+++ hotel_db.sql
@@ -1,2 +1,10 @@
 create database hotel_db;
 use hotel_db;
+
+create table reservation (
+reservation_id int auto_increment primary key,
+guest_name varchar(255) not null,
+room_number int not null,
+contact_number varchar(10) not null,
+reservation_date timestamp default current_timestamp
+);
