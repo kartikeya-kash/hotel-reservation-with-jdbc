78d929a3910b46904949ca86672ce21048ab3ca2
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-15 16:28:59 +0530

    imported lib

--- hotelReservationSystem.java
+++ hotelReservationSystem.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
 public class hotelReservationSystem {
     public static void main (String args[]){
-        
+
     }
 }
